abc3b68a46a7bfeb73feba9485571bbbf54d125b
Michael Tio <77330912+michaeltio@users.noreply.github.com>
2025-08-21 23:03:39 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,75 @@
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
 import Image from "next/image";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export default function Login() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm();
   return (
-    <main className="min-h-screen">
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center">
-          <Image
-            src={"/logo-default.png"}
-            alt="/"
-            width={"100"}
-            height={"100"}
-          />
-          <p className="font-montserrat">Please sign in your account</p>
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="w-1/4">
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col items-center justify-center">
+            <Image
+              src={"/logo-default.png"}
+              alt="/"
+              width={"50"}
+              height={"50"}
+            />
+            <p className="font-montserrat text-lg ">
+              Please sign in your account
+            </p>
+          </div>
+          <Card className="w-">
+            <CardHeader>
+              <CardTitle className="text-lg text-center">Sign In</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <form action="">
+                <div className="flex flex-col">
+                  <div>
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="youremail@example.com"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <Label htmlFor="password">Password</Label>
+                    <Input
+                      id="password"
+                      type="password"
+                      placeholder="********"
+                      required
+                    />
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </CardFooter>
+          </Card>
         </div>
-        <div>form section</div>
       </div>
     </main>
   );
