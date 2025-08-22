f192f30763f07ae59f71668d12d55461022ad61d
Michael Tio <77330912+michaeltio@users.noreply.github.com>
2025-08-22 17:21:36 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,12 @@
 import Image from "next/image";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import LoginForm from "@/components/common/forms/loginForm";
 
 export default function Login() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <div className="w-1/4">
+    <main className="h-screen flex items-center justify-center">
+      <div className="w-full lg:w-1/4 px-4 lg:px-0">
         <div className="flex flex-col gap-8">
           <div className="flex flex-col items-center justify-center">
             <Image
@@ -26,7 +20,7 @@ export default function Login() {
             </p>
             <p className="font-montserrat text-lg ">Client Portal</p>
           </div>
-          <Card className="w-">
+          <Card className="">
             <CardHeader>
               <CardTitle className="text-lg text-center">
                 Login to your account
